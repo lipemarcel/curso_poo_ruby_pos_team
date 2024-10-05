6b70e4a17d7bb0b96040d3624753cffd34ea8922
natsayuri <natsayuri@cloudwalk.io>
2024-10-05 16:21:50 -0300

    Update class10_definindo_metodos_natsayuri.rb

--- Exercises/class10_definindo_metodos_natsayuri.rb
+++ Exercises/class10_definindo_metodos_natsayuri.rb
@@ -1,7 +1,18 @@
 class Pessoa
     def gritar
+        puts "Grrrrrrr"
     end
 
     def agradecer
+        puts "obrigada!!"
     end
 end
+
+
+
+
+############
+
+obj1 = Pessoa.new
+obj1.gritar
+obj2.agradecer
